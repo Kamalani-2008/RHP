de349eab273bb2c3ff07760ec88b0bd860712afc
G.Kamalani Sri <25cs055@rmkec.ac.in>
2026-06-26 23:09:18 +0530

    Update TrappingRainWater.java

--- TrappingRainWater.java
+++ TrappingRainWater.java
@@ -10,20 +10,16 @@ public class TrappingRainWater {
         int[] rightMax = new int[n];
 
         int water = 0;
-
-        // Left maximum array
         leftMax[0] = height[0];
         for (int i = 1; i < n; i++) {
             leftMax[i] = Math.max(leftMax[i - 1], height[i]);
         }
 
-        // Right maximum array
         rightMax[n - 1] = height[n - 1];
         for (int i = n - 2; i >= 0; i--) {
             rightMax[i] = Math.max(rightMax[i + 1], height[i]);
         }
 
-        // Calculate trapped water
         for (int i = 0; i < n; i++) {
             water += Math.min(leftMax[i], rightMax[i]) - height[i];
         }
