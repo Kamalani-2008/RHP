2656f28e56aafe53d7538f98dbe53093d11d3a11
G.Kamalani Sri <25cs055@rmkec.ac.in>
2026-06-26 22:38:09 +0530

    Update AdjacentCellsSum.java

--- AdjacentCellsSum.java
+++ AdjacentCellsSum.java
@@ -1,41 +1,26 @@
 import java.util.Scanner;
-
 public class AdjacentCellsSum {
-
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
         int rows = sc.nextInt();
         int cols = sc.nextInt();
-
         int[][] matrix = new int[rows][cols];
-
-        // Input matrix
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 matrix[i][j] = sc.nextInt();
             }
         }
-
-        // Cell whose adjacent sum is to be calculated
         int r = sc.nextInt();
         int c = sc.nextInt();
-
         int sum = 0;
-
-        // 8 possible adjacent directions
         int[][] adj = {
                 {-1, -1}, {-1, 0}, {-1, 1},
                 {0, -1},           {0, 1},
                 {1, -1},  {1, 0},  {1, 1}
         };
-
         for (int i = 0; i < 8; i++) {
-
             int row = r + adj[i][0];
             int col = c + adj[i][1];
-
             if (row >= 0 && row < rows && col >= 0 && col < cols) {
                 sum += matrix[row][col];
             }
